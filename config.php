436c9b9b442c92ef0a537f50f4250daa551a4e5a
Chirag Gupta <96005208+chirag1106@users.noreply.github.com>
2022-05-12 09:56:25 +0530

    Update config.php

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 
 require_once 'error.php';
 
-$con = new mysqli('localhost','root','','medicine');
+$con = new mysqli('127.0.0.1','localhost@root','root','medicine');
 
 if($con->connect_error){
     die('connection failed'.$con->connect_errno);
